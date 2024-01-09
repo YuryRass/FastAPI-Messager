16f8dd5223218aa73ee7f9db3e31a9974e9dd430
Yurii <yurchik1997yurchik@gmail.com>
2024-01-09 19:51:35 +0300

    modified

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-from pages.router import router as page_router
 from chat.router import router as chat_router
+from crontab import one_minute_message
+from pages.router import router as page_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    one_minute_message.start()
+    yield
+    one_minute_message.stop()
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(page_router)
 app.include_router(chat_router)
