03a459389cb0f089388460aeab53b1ff784f3d7c
Yurii <yurchik1997yurchik@gmail.com>
2024-01-11 21:05:04 +0300

    Added consumer work

--- external__main/app.py
+++ external__main/app.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -5,11 +6,14 @@ from fastapi import FastAPI
 from chat.router import router as chat_router
 from cron import one_minute_message
 from pages.router import router as page_router
+from consumer.subscriptions import consumer_subscriptions
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     one_minute_message.start()
+    loop = asyncio.get_event_loop()
+    loop.create_task(consumer_subscriptions())
     yield
     one_minute_message.stop()
 
