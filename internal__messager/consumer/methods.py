7232bb81c092f06567bc7c0ac6931bbf87921687
Yurii <yurchik1997yurchik@gmail.com>
2024-01-13 22:27:06 +0300

    modified method chat_message

--- internal__messager/consumer/methods.py
+++ internal__messager/consumer/methods.py
@@ -14,16 +14,27 @@ async def simple_message_with_ack(message: DeliveredMessage):
 
 
 async def chat_message(message: DeliveredMessage):
-    incoming_message_dict = json.loads(message.body)
-    incoming_message = incoming_message_dict["message"]
+    incoming_message: dict = json.loads(message.body)
+    incoming_message: str = incoming_message["message"]
 
-    if len(incoming_message) > 5 and incoming_message[-4:] == "!pow":
-        pass
+    if (
+        incoming_message.endswith('!pow')
+        and incoming_message["source"] == "external__main"
+    ):
+        outcoming_message: dict = {}
+        outcoming_message["username"] = "internal_messager"
+        outcoming_message["message"] = incoming_message
+        await producer_methods.send_pow_message_to_internal_worker(
+            outcoming_message
+        )
+        await message.channel.basic_ack(message.delivery.delivery_tag)
     else:
-        outcoming_message = incoming_message[::-1]
-
-    outcoming_message_dict = {}
-    outcoming_message_dict["username"] = "internal_messager"
-    outcoming_message_dict["message"] = outcoming_message
-    await producer_methods.send_message_to_external_main(outcoming_message_dict)
-    await message.channel.basic_ack(message.delivery.delivery_tag)
+        if incoming_message["source"] == "external__main":
+            outcoming_message = incoming_message[::-1]
+        elif incoming_message["source"] == "internal__worker":
+            outcoming_message = incoming_message
+        outcoming_message: dict = {}
+        outcoming_message["username"] = "internal_messager"
+        outcoming_message["message"] = outcoming_message
+        await producer_methods.send_message_to_external_main(outcoming_message)
+        await message.channel.basic_ack(message.delivery.delivery_tag)
