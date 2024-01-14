b4a44ce3978769fc5980c05a997a897737158a17
Yurii <yurchik1997yurchik@gmail.com>
2024-01-14 16:04:05 +0300

    added rps subscription in the event loop

--- internal__worker/app.py
+++ internal__worker/app.py
@@ -2,12 +2,14 @@ import asyncio
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from consumer.subscriptions import consumer_subscriptions
+from rpc.subscriptions import rpc_subscriptions
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     loop = asyncio.get_event_loop()
     loop.create_task(consumer_subscriptions())
+    loop.create_task(rpc_subscriptions())
     yield
 
 
